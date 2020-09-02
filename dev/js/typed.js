a083f0fa310e03fe54af7835c6e1e6eb6675298c
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2020-09-02 00:50:37 -0400

    type js index checker

--- dev/js/typed.js
+++ dev/js/typed.js
@@ -1,14 +1,15 @@
 // Can also be included with a regular script tag
 //import Typed from 'typed.js';
+if(indexChecker){
+  var options = {
+    strings: ['Campaign work provides a multitude of opportunities to the designer', 'Scroll down to see more!'],
+    typeSpeed: 5,
+    backSpeed: 5,
+    startDelay: 3000,
+    backDelay: 2000,
+    fadeOut: true,
+    loop: true
+  };
 
-var options = {
-  strings: ['Campaign work provides a multitude of opportunities to the designer', 'Scroll down to see more!'],
-  typeSpeed: 5,
-  backSpeed: 5,
-  startDelay: 3000,
-  backDelay: 2000,
-  fadeOut: true,
-  loop: true
-};
-
-var typed = new Typed('.typejs', options);
+  var typed = new Typed('.typejs', options);
+}
